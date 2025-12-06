fea91b6a378023d294e363461b90a69dcb2b2467
Sanyog Jadhav <jadhavsanyog.400@gmail.com>
2025-12-06 14:33:23 +0530

    first commit changeserver

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000", // Frontend URL
+    origin: ["http://localhost:3000", "https://krushi-sarjana.vercel.app"], // Frontend URLs
     credentials: true, // Allows sending cookies across origins
   })
 );
